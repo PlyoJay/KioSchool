6bc565528fbb6e7e089f700ccaa202ae6a94c8ce
HyungJune Kim <hj615@cuuva.com>
2025-06-16 14:34:58 +0900

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KioSchool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
